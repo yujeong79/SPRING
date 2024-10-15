c368361bfca0e3f96aaedf720ed9fe6405d93b14
김유정 <aabbc0908@naver.com>
2024-10-15 17:42:30 +0900

    commit

---
+++ spring_ws_03_2_SpringBoot/src/main/java/com/ssafy/mvc/SpringWs032SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWs032SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWs032SpringBootApplication.class, args);
+	}
+
+}
