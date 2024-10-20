9ead38b4eba6d9d47a79587658d0ff02932c7d60
yujeong <aabbc0908@naver.com>
2024-10-20 15:01:43 +0900

    commit

---
+++ spring_ws_04_2_SpringBoot/src/main/java/com/ssafy/mvc/SpringWs042SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWs042SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWs042SpringBootApplication.class, args);
+	}
+
+}
