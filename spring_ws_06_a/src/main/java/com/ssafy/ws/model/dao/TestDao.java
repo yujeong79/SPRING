cfb6b1d8c0686b351ceff91e32b44c5ac8cdd041
김유정 <aabbc0908@naver.com>
2024-10-21 11:05:44 +0900

    commit

--- spring_ws_06_a/src/main/java/com/ssafy/ws/model/dao/TestDao.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.ws.model.dao;
-
-import com.ssafy.ws.model.dto.DateDto;
-
-public interface TestDao {
-	public DateDto getCurrentDate() throws Exception;
-}
--- spring_ws_06_a/src/main/java/com/ssafy/ws/model/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ws.model.service;
-
-import com.ssafy.ws.model.dto.DateDto;
-
-public interface TestService {
-
-	DateDto test() throws Exception;
-
-}
