ee7f04ee478597273cd8ef412ab1c9df1eb344c2
김유정 <aabbc0908@naver.com>
2024-10-24 11:13:48 +0900

    commit

---
+++ Spring_Day11_RESTAPI/src/main/java/com/ssafy/mvc/SpringDay11RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDay11RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDay11RestapiApplication.class, args);
+	}
+
+}
